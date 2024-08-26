5bf6cad025721dedccfe034cc4190f9a8ffa25be
Leory2 <pexoleo@gmail.com>
2024-08-26 17:43:34 -0300

    update

---
+++ CursoleoC-123-main/02-TiposPrimitivos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    public static class TiposPrimitivos
+    {
+        public static void ImprimirTipos()
+        {
+
+            sbyte SByte = -128; // valores variam de -128 a 127
+            Console.WriteLine($"sbyte: {SByte}");
+
+            byte variavelByte = 255; // valores variam de 0 a 255
+            Console.WriteLine($"byte: {variavelByte}");
+
+            short variavelShort = -32768; // valores variam de -32768 a 32767
+            Console.WriteLine($"short: {variavelShort}");
+
+            ushort variavelUShort = 65535; // valores variam de 0 a 65535
+            Console.WriteLine($"ushort: {variavelUShort}");
+
+            int variavelInt = -2147483648; // valores variam de -2147483648 a 2147483647
+            Console.WriteLine($"int: {variavelInt}");
+
+            uint variavelUInt = 4294967295; // valores variam de 0 a 4294967295
+            Console.WriteLine($"uint: {variavelUInt}");
+
+            long variavelLong = -9223372036854775808; // valores variam de -9223372036854775808 a 
+            Console.WriteLine($"long: {variavelLong}");
+
+            ulong variavelULong = 18446744073709551615; // valores variam de 0 a  18446744073709551615
+            Console.WriteLine($"ulong: {variavelULong}");
+
+            float variavelFloat = 3.14f;
+            Console.WriteLine($"float: {variavelFloat}");
+
+            double variavelDouble = 3.141592653589793;
+            Console.WriteLine($"double: {variavelDouble}");
+
+            decimal variavelDecimal = 79228162514264337593543950335M;
+            Console.WriteLine($"decimal: {variavelDecimal}");
+
+            char variavelChar = 'A';
+            Console.WriteLine($"char: {variavelChar}");
+
+            bool variavelBool = true;
+            Console.WriteLine($"bool: {variavelBool}"); // valores são true ou false
+
+            object variavelObject = null;
+            Console.WriteLine($"object: {variavelObject}"); // pode conter qualquer tipo de valor
+
+            string variavelString = "Olá, mundo!";
+            Console.WriteLine($"string: {variavelString}"); // uma cadeia de caracteres
+
+        }
+    }
+}
---
+++ CursoleoC-123-main/Program.cs
@@ -0,0 +1,93 @@
+﻿using CursoCSharp;
+using CursoCSharp.Exercicios;
+//bool resultado;
+//resultado = _04_OperadoresLogicos.ELogico(false, true);
+//Console.WriteLine($"O resultado do operador logico E:{resultado}");
+//resultado = _04_OperadoresLogicos.Negacao(true);
+//Console.WriteLine($"O resultado do operador logico Negação:{resultado}");
+//resultado = _04_OperadoresLogicos.OULogico(true, true);
+//Console.WriteLine($"O resultado do operador logico OULogico:{resultado}");
+//resultado = _04_OperadoresLogicos.OUExclusivo(true, true);
+//Console.WriteLine($"O resultado do operador logico OUExclusivo:{resultado}");
+//OperadoresComparacao.DemonstrarOperadores()
+
+//_07_OperadorTernario.DemonstrarTernario();
+//_08_Conversao.DemonstrarConversoes();
+//_10_EstruturaRepeticao.DemonstrarRepeticoes();
+//_11_ArrayExemplo.DemonstrarArray();
+//_12_ListExemplo.DemonstrarLista();
+
+//Aluno vitor = new Aluno();
+//Aluno atila = new Aluno();
+//vitor.nome = "João Vitor";
+//vitor.matricula = "1010100010101";
+//vitor.curso = "Tecnico";
+//atila.nome = "Atila Cantoara";
+//atila.matricula = "991919191119";
+//atila.curso = "Tecnico";
+
+//Console.WriteLine($"O nome do aluno é: {vitor.nome}, o curso do aluno é: {vitor.curso}, a matricula do aluno é: {vitor.matricula}");
+//Console.WriteLine($"O nome do aluno é: {atila.nome}, o curso do aluno é: {atila.curso}, a matricula do aluno é: {atila.matricula}");
+
+//_13_EscopoVariavel es = new _13_EscopoVariavel();
+
+//es.Metodo1();
+//es.Metodo2();
+//es.Metodo3();
+//es.ExibirContadorGlobal();
+
+//_15_TratarExcessoes tr = new _15_TratarExcessoes();
+//tr.ExecutarDivisao(9, 3);
+//_15_TratarExcessoes tr1 = new _15_TratarExcessoes();
+//tr1.ExecutarDivisao(2, 0);
+
+//Exercicios2 ex = new Exercicios2();
+//ex.CalculaSoma(2, 3);
+
+//_16_ConceitosPOO Conceitos = new _16_ConceitosPOO();
+//Conceitos.ExplicarOrientacaoAObjetos();
+//Conceitos.DiferencaEntreParadigmas();
+//Conceitos.ExplicarPilares();
+
+//1. Criando objetos (instânciando a classe pessoas)
+//Pessoa pessoa1 = new Pessoa("Alice", 30);
+//Pessoa pessoa2 = new Pessoa("Bob", 25);
+
+////2. Usando métodos de instância (métodos não estatísticos)
+//pessoa1.ExibirInformacoes();
+//pessoa2.ExibirInformacoes();
+
+////3. Usando método estático (Pertence á classe, não á instância)
+//Pessoa.ExibirNumeroDePessoas();
+
+////4. Membros de Intância vs. Membros Estáticos
+//Console.WriteLine($"Pessoa1: {pessoa1.Nome}, idade: {pessoa1.Idade}");
+//Console.WriteLine($"Pessoa1: {pessoa2.Nome}, idade: {pessoa2.Idade}");
+//Console.WriteLine($"número tatal de pessoas: {Pessoa.NumeroDePessoas}");
+
+//Carro carro = new Carro("Honda", "Civic",1997);
+
+//carro.ExibirDetalhes();
+
+//Pessoas pessoas = new Pessoas("Bianca", 27, "Brasília");
+//pessoas.ExibirinFormações();
+
+//Ex6 ex6 = new Ex6();
+//ex6.EntradaSaida();
+
+//Exercicio7 resul = new Exercicio7();
+//resul.ExibirDetales(5, 10);
+
+//exercicio8.ExibirDetalhes();
+
+//exercicio9 resul = new exercicio9();
+//resul.VerificarMaioridade(18);
+
+//exercicio10.ExibirAltura();
+
+//exercicio11 resul = new exercicio11();
+//resul.CalcularArea(5);
+
+
+
+
